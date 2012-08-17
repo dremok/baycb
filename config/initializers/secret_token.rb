e445b6e9fe7ee31fb9ada4100345001b36aca9b4
Max Leander <max.y.leander@gmail.com>
2012-08-17 13:19:35 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Baycb::Application.config.secret_token = '61bed54d63e1f9e9837ba2aaf6374676e7593c97ba330457689a71ca5fa32773f7eba45d5858238042e25fcb8a7359be85f15776c257f02f6e54f8a20f5a6488'
