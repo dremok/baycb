e60e93fbca999db58aeaa8ec74970bca76df6e35
Max Leander <max.y.leander@gmail.com>
2012-08-18 10:07:41 +0200

    Some more on static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,25 +3,38 @@ require 'spec_helper'
 describe "StaticPages" do
   
   describe "Home page" do
-    it "should have the content 'Be All You Can Be'" do
+    it "should have the h1 'Be All You Can Be'" do
       visit '/static_pages/home'
-      page.should have_content('Be All You Can Be')
+      page.should have_selector('h1', :text => "Be All You Can Be")
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Be All You Can Be | Home")
     end
   end
   
   describe "Help page" do
-    
-    it "should have the content 'Help'" do
+    it "should have the h1 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => "Help")
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "Be All You Can Be | Help")
     end
   end
   
-    describe "About page" do
-    
-    it "should have the content 'About Us'" do
+  describe "About page" do
+    it "should have the h1 'About Us'" do
       visit '/static_pages/about'
-      page.should have_content('About Us')
+      page.should have_selector('h1', :text => "About Us")
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "Be All You Can Be | About Us")
     end
   end
   
